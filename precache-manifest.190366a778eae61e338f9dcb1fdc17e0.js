8b0ce32babd80e3b64b8ae1f70550571bcf8f839
Gaurav Goswami <gouravgoswami48@gmail.om>
2019-10-18 11:52:10 +0530

    Updates

---
+++ precache-manifest.190366a778eae61e338f9dcb1fdc17e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe67cfd4e3ae4002aa7e09ae374a828",
+    "url": "/farm-fresh/index.html"
+  },
+  {
+    "revision": "2fea6f9cfe356fe68837",
+    "url": "/farm-fresh/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3b3c5553af7be709138",
+    "url": "/farm-fresh/static/js/2.6412dfc6.chunk.js"
+  },
+  {
+    "revision": "2fea6f9cfe356fe68837",
+    "url": "/farm-fresh/static/js/main.424c0107.chunk.js"
+  },
+  {
+    "revision": "a4f28e6243bde3be5718",
+    "url": "/farm-fresh/static/js/runtime-main.41136521.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/farm-fresh/static/media/logo.25bf045c.svg"
+  }
+]);
